54dfe8a6c1abaa7da733f87fe0dbd6be1f26b47e
Nuno Góis <nunojosegois@gmail.com>
2019-05-22 09:59:14 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+/* REQUIRES */
+const express = require('express');
+const bodyparser = require('body-parser');
+require('dotenv').config();
+
+const app = express(); 
+
+/* USES */
+app.use(bodyparser.json());
+
+/* ROUTES */
+app.get('/', function(req, res) {
+	res.send(process.env.SECRET);
+});
+
+app.listen(3000);
