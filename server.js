5854990e4d22038208b839d51c6bf8fb92df5424
Nuno Góis <nunojosegois@gmail.com>
2019-05-22 11:24:02 +0100

    Google Auth

--- server.js
+++ server.js
@@ -1,16 +1,39 @@
 /* REQUIRES */
 const express = require('express');
 const bodyparser = require('body-parser');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 require('dotenv').config();
 
-const app = express(); 
+const app = express();
+
+passport.use(new GoogleStrategy({
+		clientID: process.env.GOOGLE_CLIENT_ID,
+		clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+		callbackURL: "https://wishlist-quasar-api.herokuapp.com/auth/google/callback"
+	},
+	function(accessToken, refreshToken, profile, cb) {
+		User.findOrCreate({ googleId: profile.id }, function (err, user) {
+			return cb(err, user);
+		});
+	}
+));
+
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), function(req, res) {
+	// Successful authentication, redirect home.
+	res.redirect('/');
+});
+
 
 /* USES */
 app.use(bodyparser.json());
 
 /* ROUTES */
 app.get('/', function(req, res) {
-	res.send(process.env.SECRET);
+	res.send('Welcome to the Wishlist API! "Make your wishes come true!" - Someone');
 });
 
 app.listen(process.env.PORT || 3000);
