b414f9cccb2fac88c7cb511f32568a46dedb4f2d
Nuno Góis <nunojosegois@gmail.com>
2019-05-24 15:51:53 +0100

    added socket.io!

--- server.js
+++ server.js
@@ -14,6 +14,22 @@ const session = require('./session');
 
 const app = express();
 
+/* SOCKET.IO */
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const iojwt = require('socketio-jwt');
+
+let io_users = [];
+
+io.use(iojwt.authorize({
+  secret: process.env.JWT_SECRET,
+  handshake: true
+}));
+
+io.on('connection', function (socket) {
+	socket.join(socket.decoded_token.id);
+})
+
 /* USES */
 app.use(bodyparser.json());
 app.use(cors());
@@ -41,6 +57,7 @@ app.get('/load', session.check, async function(req, res) {
 app.post('/save', session.check, async function(req, res) {
 	const new_list = { id: req.user.id, items: req.body.items, updated: new Date().toISOString() };
 	await list.findOneAndUpdate({ id: req.user.id }, new_list, { upsert: true });
+	io.to(req.user.id).emit('save', req.body.items);
 	res.sendStatus(200);
 })
 
@@ -49,4 +66,4 @@ app.post('/logout', function (req, res) {
 	res.sendStatus(200);
 })
 
-app.listen(process.env.PORT || 3000);
+http.listen(process.env.PORT || 3000);
